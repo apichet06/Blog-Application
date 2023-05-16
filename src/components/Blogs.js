bc42962b5abcdccecab995c0f4450b56398167d6
apichet06 <apichet06@gmail.com>
2023-05-16 13:49:27 +0700

    7rd Update

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,41 +1,49 @@
-import { Container, Row, Figure, Col, Form } from "react-bootstrap";
-import blogs from "../data/blogs";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+// import { Container, Row, Figure, Col, Form } from "react-bootstrap";
+// import blogs from "../data/blogs";
+// import { Link } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// export const Blogs = () => {
+//     const [search, setSearch] = useState("");
+//     const [filterBlog, setFilterBlog] = useState([])
+//     useEffect(() => {
+//         const result = blogs.filter((item) => item.title.includes(search))
+//         setFilterBlog(result);
+//         // eslint-disable-next-line
+//     }, []);
+//     return (
+//         <Container>
+//             <Row className="justify-content-center">
+//                 <Col md={6}>
+//                     <Form.Group className="mb-3" controlId="search">
+//                         <Form.Control type="text" placeholder="ค้นหาทบความ" autoComplete="off"
+//                             value={search}
+//                             onChange={e => setSearch(e.target.value)} />
+//                     </Form.Group>
+//                 </Col>
+//                 {
+//                     filterBlog.map((blog) => (
+//                         <Col md="12" key={blog.id}>
+//                             <Figure>
+//                                 <Figure.Caption>
+//                                     <Link to={`/blog/${blog.id}`} style={{ textDecoration: 'none' }} className="text-secondary">
+//                                         <h3>{blog.title}</h3>
+//                                         {blog.content.substring(0, 400)}
+//                                     </Link>
+//                                     <hr />
+//                                 </Figure.Caption>
+//                             </Figure>
+//                         </Col>
+//                     ))
+//                 }
+//             </Row>
+//         </Container>
+//     )
+// }
+
+import React from 'react'
+
 export const Blogs = () => {
-    const [search, setSearch] = useState("");
-    const [filterBlog, setFilterBlog] = useState([])
-    useEffect(() => {
-        const result = blogs.filter((item) => item.title.includes(search))
-        setFilterBlog(result);
-        // eslint-disable-next-line
-    }, []);
     return (
-        <Container>
-            <Row className="justify-content-center">
-                <Col md={6}>
-                    <Form.Group className="mb-3" controlId="search">
-                        <Form.Control type="text" placeholder="ค้นหาทบความ" autoComplete="off"
-                            value={search}
-                            onChange={e => setSearch(e.target.value)} />
-                    </Form.Group>
-                </Col>
-                {
-                    filterBlog.map((blog) => (
-                        <Col md="12" key={blog.id}>
-                            <Figure>
-                                <Figure.Caption>
-                                    <Link to={`/blog/${blog.id}`} style={{ textDecoration: 'none' }} className="text-secondary">
-                                        <h3>{blog.title}</h3>
-                                        {blog.content.substring(0, 400)}
-                                    </Link>
-                                    <hr />
-                                </Figure.Caption>
-                            </Figure>
-                        </Col>
-                    ))
-                }
-            </Row>
-        </Container>
+        <div>Blogs</div>
     )
 }
